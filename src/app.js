7cf8d35def5c533b271d8856c4e7dad98764c28f
User <user@users-mbp.home>
2020-02-17 19:51:38 +0000

    now can mark existing item as complete

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      items: 
-      ["Buy Food", "Clean House"]
+      items: [
+        {name: "Buy Food", isComplete: true},
+        {name: "Clean House", isComplete: false},
+        {name: "Book Holiday", isComplete: true }
+      ]
+    },
+    methods: {
+      saveNewItem: function() {
+        this.items.push({
+          name: this.newItem,
+          isComplete: false
+        });
+        this.newItem = "";
+      },
+      completeItem: function(index) {
+        this.items[index].isComplete = true;
+      }
     }
   });
 });
